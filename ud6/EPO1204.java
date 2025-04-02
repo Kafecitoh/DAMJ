0456e57cd945a31449ed07271c013a19a2ab0a13
Kafecitoh <asantmaga@ieschandomonte.edu.es>
2025-04-02 11:13:56 +0200

    ContornosBadSmells

---
+++ ud6/EPO1204.java
@@ -0,0 +1,26 @@
+package ud6;
+//E1204. Implementar una aplicación que pida por consola números enteros no negativos
+//hasta que se introduce un -1. Los números se irán insertando en una colección,
+//pudiéndose repetir. Al terminar, se mostrará la colección por pantalla.
+//A continuación, se mostrarán todos los valores pares. Por último, se eliminarán todos los
+//múltiplos de 3 y se mostrará por pantalla la colección resultante.
+//Practica el uso de iteradores y bucles for-each para recorrer la colección.
+
+import java.util.Scanner;
+
+public class EPO1204 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros =  new int [100]; //suponemos un maximo de 100 numeros
+        int cuenta = 0;
+
+        System.out.println(" Introduce de numeros hasta que se ingrese -1");
+        int numerin;
+      //  while ((numerin == scanner.nextInt()) != -1){
+         //   if (numerin >= 0 %% cuenta < numeros.length){
+              //  numeros[cuenta++] = numerin;
+            }
+
+        }
+    
+
