1f088e11cfd6517ab4c18cd3bc6ff108ea64f745
Kafecitoh <asantmaga@ieschandomonte.edu.es>
2025-05-21 12:21:02 +0200

    Actualizar UTIL1.java

--- BBDD/UTIL1.java
+++ BBDD/UTIL1.java
@@ -1,5 +1,51 @@
-package BBDD;
+package musicollab;
+//CANCIÓN.JAVA
+import java.util.*;
 
-public class UTIL1 {
-    
+public class Cancion implements Comparable<Cancion> {
+    private String nombre;
+    private String autor;
+    private String estiloMusical;
+    private List<String> instrumentosRequeridos;
+
+    public Cancion(String nombre, String autor, String estiloMusical, List<String> instrumentosRequeridos) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.estiloMusical = estiloMusical;
+        this.instrumentosRequeridos = new ArrayList<>(new TreeSet<>(instrumentosRequeridos));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEstiloMusical() {
+        return estiloMusical;
+    }
+
+    public List<String> getInstrumentosRequeridos() {
+        return instrumentosRequeridos;
+    }
+
+    public static List<String> instrumentosDistintos(Collection<Cancion> canciones) {
+        Set<String> instrumentos = new TreeSet<>();
+        for (Cancion c : canciones) {
+            instrumentos.addAll(c.instrumentosRequeridos);
+        }
+        return new ArrayList<>(instrumentos);
+    }
+
+    @Override
+    public int compareTo(Cancion o) {
+        return this.nombre.compareToIgnoreCase(o.nombre);
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + nombre + "\" (" + autor + ") - " + estiloMusical + " | Instrumentos: " + instrumentosRequeridos;
+    }
 }
