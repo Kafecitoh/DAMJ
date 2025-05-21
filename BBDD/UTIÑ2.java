060394423bfb673b6ee1c6a7ec82ac864b0d98c7
Kafecitoh <asantmaga@ieschandomonte.edu.es>
2025-05-21 12:21:33 +0200

    Actualizar UTIÑ2.java

---
+++ BBDD/UTIÑ2.java
@@ -0,0 +1,44 @@
+package musicollab;
+
+import java.util.*;
+//MUSICO
+
+public class Musico implements Comparable<Musico> {
+    private String nombre;
+    private String alias;
+    private List<String> instrumentos;
+    private List<Cancion> repertorio;
+
+    public Musico(String nombre, String alias, List<String> instrumentos, List<Cancion> repertorio) {
+        this.nombre = nombre;
+        this.alias = alias;
+        this.instrumentos = new ArrayList<>(new TreeSet<>(instrumentos));
+        this.repertorio = repertorio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public List<String> getInstrumentos() {
+        return instrumentos;
+    }
+
+    public List<Cancion> getRepertorio() {
+        return repertorio;
+    }
+
+    @Override
+    public int compareTo(Musico o) {
+        return this.alias.compareToIgnoreCase(o.alias);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + alias + "] (" + nombre + "): " + instrumentos + " | Repertorio: " + repertorio.size() + " canciones";
+    }
+}
